8ee1d026738f0e995ed8c05568c8b48e7ae07650
root <root@noithat-thanhkhanh.ga>
2016-09-11 10:03:25 +0000

    2

--- wp-content/themes/shop-isle/header.php
+++ wp-content/themes/shop-isle/header.php
@@ -7,15 +7,14 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> <?php shop_isle_html_tag_schema(); ?>>
 <head>
-<link rel="canonical" href="http://noithat-thanhkhanh.ga" />
-<link rel="alternate" href="http://noithat-thanhkhanh.ga" hreflang="vi-vn" />
-
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
+<link rel="canonical" href="http://noithat-thanhkhanh.ga" />
+<link rel="alternate" href="http://noithat-thanhkhanh.ga" hreflang="vi-vn" />
 </head>
 
 <body <?php body_class(); ?>>
